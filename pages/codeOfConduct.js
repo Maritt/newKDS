510b41356c8ae8b98bb6658deed2a003ac88e792
kxb <kxb@kxbs-MacBook-Pro.local>
2019-07-17 10:00:54 +0200

    set up menu and pages

---
+++ pages/codeOfConduct.js
@@ -0,0 +1,10 @@
+import Layout from "./components/Layout";
+import "../styling/styles.scss";
+
+export default function CodeOfConduct() {
+  return (
+    <Layout>
+      <p>This page contains the code of conduct</p>
+    </Layout>
+  );
+}
---
+++ pages/location.js
@@ -0,0 +1,9 @@
+import "../styling/styles.scss";
+import Layout from "./components/Layout";
+export default function Location() {
+  return (
+    <Layout>
+      <p>This page contains the location</p>
+    </Layout>
+  );
+}
