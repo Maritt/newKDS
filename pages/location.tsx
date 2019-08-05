e3c4a2acdace9bc10585f05beac99500e4b93a6d
Elise Fiskeseth <elise.fiskeseth@knowit.no>
2019-08-05 09:19:41 +0200

    Added font variable and fixed map size

--- pages/location.tsx
+++ pages/location.tsx
@@ -18,9 +18,11 @@ export default function Location() {
 
 const Map = () => {
   return (
-    <iframe
-      id="map"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2035.1707535769806!2d18.053726516659214!3d59.33010298165915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d604f482201%3A0x858ff8c54ed4011b!2sRadisson+Blu+Waterfront+Hotel+Stockholm!5e0!3m2!1sno!2sno!4v1563453424921!5m2!1sno!2sno"
-    />
+    <div>
+      <iframe
+        id="map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2035.1707535769806!2d18.053726516659214!3d59.33010298165915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d604f482201%3A0x858ff8c54ed4011b!2sRadisson+Blu+Waterfront+Hotel+Stockholm!5e0!3m2!1sno!2sno!4v1563453424921!5m2!1sno!2sno"
+      />
+    </div>
   );
 };
